b3843e74d9ff20ae7520c084bc4281539fab6d55
“pakeecbala” <pakeecbala@hotmail.com>
2019-03-21 15:16:17 +0100

    update restaurantsite

---
+++ src/components/restaurants/ReviewCard.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const ReviewCard = props => {
+  return (
+    <div class="card border-secondary mb-3" style="max-width: 18rem;">
+      <div class="card-body">
+        <h4 className="card-title">{props.rating}</h4>
+        <h6 className="card-text">{props.review}</h6>
+      </div>
+    </div>
+  );
+};
+
+export default ReviewCard;
--- src/pages/Restaurants.js
+++ src/pages/Restaurants.js
@@ -2,16 +2,20 @@ import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import NavigationBar from "../components/navbar/NavbarPage";
 import RestaurantCard from "../components/restaurants/RestaurantCard";
-import dataFile from "../data.json";
+import ReviewCard from "../components/restaurants/ReviewCard";
+import dataFileRestaurant from "../dataRestaurant.json";
+import dataFileReview from "../dataReview.json";
 
 class Restaurants extends Component {
   state = {
-    restaurants: dataFile.restaurants
+    restaurants: dataFileRestaurant.restaurants,
+    reviews: dataFileReview.reviews
   };
 
   render() {
     const title = "My's Restaurants!";
     const restaurants = this.state.restaurants;
+    const reviews = this.state.reviews;
 
     return (
       <div>
@@ -37,8 +41,15 @@ function RestaurantList(props) {
       category={res.category}
     />
   ));
-
   return <div className="row">{listRestaurants}</div>;
+}
+
+function ReviewList(props) {
+  const reviews = props.reviews;
+  const listReviews = reviews.map(res => (
+    <ReviewCard rating={res.rating} review={res.review} />
+  ));
+  return <div className="row">{listReviews}</div>;
 }
 
 export default Restaurants;
