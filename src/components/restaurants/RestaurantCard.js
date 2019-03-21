0b2d268a03723f68f3087cc64515cfdb7e61d5d4
“pakeecbala” <pakeecbala@hotmail.com>
2019-03-21 13:36:45 +0100

    restaurant

--- src/components/restaurants/RestaurantCard.js
+++ src/components/restaurants/RestaurantCard.js
@@ -3,9 +3,17 @@ import React from "react";
 const RestaurantCard = props => {
   return (
     <div className="col-xs-12 col-sm-6 col-md-4">
-      <div className="card">
+      <div className="card text-white bg-info mb-3">
+        <div className="card-header">
+          <div>
+            <button>Edit</button>
+          </div>
+        </div>
         <div className="card-body">
           <h4 className="card-title">{props.name}</h4>
+          <h6 className="card-text">{props.address}</h6>
+          <h6 className="card-text">Description: {props.description}</h6>
+          <h6 className="card-text">Category: {props.category}</h6>
           <div className="mapouter">
             <div className="gmap_canvas">
               <iframe
@@ -25,7 +33,9 @@ const RestaurantCard = props => {
               />
             </div>
           </div>
-          <p>Edit</p>
+          <div>
+            <h4>Reviews</h4>
+          </div>
         </div>
       </div>
     </div>
--- src/pages/Restaurants.js
+++ src/pages/Restaurants.js
@@ -2,17 +2,43 @@ import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import NavigationBar from "../components/navbar/NavbarPage";
 import RestaurantCard from "../components/restaurants/RestaurantCard";
+import dataFile from "../data.json";
 
 class Restaurants extends Component {
+  state = {
+    restaurants: dataFile.restaurants
+  };
+
   render() {
+    const title = "My's Restaurants!";
+    const restaurants = this.state.restaurants;
+
     return (
       <div>
-        <NavigationBar />
-        <div className="app container" />
-        <h1>Welcome to restaurant</h1>
+        <div>
+          <NavigationBar />
+        </div>
+        <h2>{title}</h2>
+        <RestaurantList restaurants={restaurants} />
       </div>
     );
   }
+}
+
+function RestaurantList(props) {
+  const restaurants = props.restaurants;
+  const listRestaurants = restaurants.map(res => (
+    <RestaurantCard
+      key={res.id}
+      id={res.id}
+      name={res.name}
+      address={res.address}
+      description={res.description}
+      category={res.category}
+    />
+  ));
+
+  return <div className="row">{listRestaurants}</div>;
 }
 
 export default Restaurants;
