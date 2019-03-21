24aadc72832471e51ba1325a502387b54aec83e0
Kenneth Ngo <kenneth.ngo@no.experis.com>
2019-03-21 12:42:45 +0100

    Added login component

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -1,22 +1,47 @@
 import React, { Component } from "react";
 import NavBar from "../navbar/NavbarPage";
-
+import { Form, Button, Card, Nav } from "react-bootstrap";
 class Login extends Component {
   render() {
     return (
       <div>
         <NavBar />
-        <div className="app container">
-          <h2>HELLO</h2>
-          <p>
-            Cras facilisis urna ornare ex volutpat, et convallis erat elementum.
-            Ut aliquam, ipsum vitae gravida suscipit, metus dui bibendum est,
-            eget rhoncus nibh metus nec massa. Maecenas hendrerit laoreet augue
-            nec molestie. Cum sociis natoque penatibus et magnis dis parturient
-            montes, nascetur ridiculus mus.
-          </p>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "50vh"
+          }}
+        >
+          <Card bg="light" text="black" style={{ width: "18rem" }}>
+            <Card.Header>
+              <Nav variant="pills" defaultActiveKey="#first">
+                <Nav.Item>
+                  <Nav.Link href="#first">Login</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link href="#link">Register</Nav.Link>
+                </Nav.Item>
+              </Nav>
+            </Card.Header>
+            <Card.Body>
+              <Form>
+                <Form.Group controlId="formBasicEmail">
+                  <Form.Label>Username</Form.Label>
+                  <Form.Control type="username" placeholder="Username" />
+                </Form.Group>
+                <Form.Group controlId="formBasicPassword">
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control type="password" placeholder="Password" />
+                </Form.Group>
 
-          <p>Duis a turpis sed lacus dapibus elementum sed eu lectus.</p>
+                <Button variant="dark" type="submit">
+                  Sign In
+                </Button>
+              </Form>
+            </Card.Body>
+          </Card>
         </div>
       </div>
     );
