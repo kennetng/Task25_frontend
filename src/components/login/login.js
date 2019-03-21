bc69d82c0cc57390f1452ef7a8c18948526ab8a2
Kenneth Ngo <kenneth.ngo@no.experis.com>
2019-03-21 13:31:38 +0100

    Fixed login to scale and position

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -1,7 +1,14 @@
 import React, { Component } from "react";
 import NavBar from "../navbar/NavbarPage";
-import { Form, Button, Card, Nav } from "react-bootstrap";
+import { Form, Button, Card, Tab, Tabs, Nav, Row, Col } from "react-bootstrap";
 class Login extends Component {
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      key: "home"
+    };
+  }
+
   render() {
     return (
       <div>
@@ -10,38 +17,98 @@ class Login extends Component {
           style={{
             display: "flex",
             justifyContent: "center",
-            alignItems: "center",
+            padding: "10vh",
             height: "50vh"
           }}
         >
-          <Card bg="light" text="black" style={{ width: "18rem" }}>
-            <Card.Header>
-              <Nav variant="pills" defaultActiveKey="#first">
-                <Nav.Item>
-                  <Nav.Link href="#first">Login</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                  <Nav.Link href="#link">Register</Nav.Link>
-                </Nav.Item>
-              </Nav>
-            </Card.Header>
-            <Card.Body>
-              <Form>
-                <Form.Group controlId="formBasicEmail">
-                  <Form.Label>Username</Form.Label>
-                  <Form.Control type="username" placeholder="Username" />
-                </Form.Group>
-                <Form.Group controlId="formBasicPassword">
-                  <Form.Label>Password</Form.Label>
-                  <Form.Control type="password" placeholder="Password" />
-                </Form.Group>
-
-                <Button variant="dark" type="submit">
-                  Sign In
-                </Button>
-              </Form>
-            </Card.Body>
-          </Card>
+          <Tab.Container id="left-tabs-example" defaultActiveKey="first">
+            <Row>
+              <Col sm={3}>
+                <Nav variant="pills" className="flex-column">
+                  <Nav.Item>
+                    <Nav.Link eventKey="first">Login</Nav.Link>
+                  </Nav.Item>
+                  <Nav.Item>
+                    <Nav.Link eventKey="second">Register</Nav.Link>
+                  </Nav.Item>
+                </Nav>
+              </Col>
+              <Col sm={9}>
+                <Tab.Content>
+                  <Tab.Pane eventKey="first">
+                    <Card bg="light" text="black" style={{ width: "18rem" }}>
+                      <Card.Body>
+                        <Form>
+                          <Form.Group controlId="formBasicEmail">
+                            <Form.Label>Username</Form.Label>
+                            <Form.Control
+                              type="username"
+                              placeholder="Username"
+                            />
+                          </Form.Group>
+                          <Form.Group controlId="formBasicPassword">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control
+                              type="password"
+                              placeholder="Password"
+                            />
+                          </Form.Group>
+                          <div
+                            style={{
+                              display: "flex",
+                              justifyContent: "center",
+                              alignItems: "center"
+                            }}
+                          >
+                            <Button variant="dark" type="submit">
+                              Sign In
+                            </Button>
+                          </div>
+                        </Form>
+                      </Card.Body>
+                    </Card>
+                  </Tab.Pane>
+                  <Tab.Pane eventKey="second">
+                    <Card bg="light" text="black" style={{ width: "18rem" }}>
+                      <Card.Body>
+                        <Form>
+                          <Form.Group controlId="formBasicEmail">
+                            <Form.Label>Username</Form.Label>
+                            <Form.Control
+                              type="username"
+                              placeholder="Username"
+                            />
+                          </Form.Group>
+                          <Form.Group controlId="formBasicPassword">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control
+                              type="password"
+                              placeholder="Password"
+                            />
+                          </Form.Group>
+                          <Form.Group controlId="formBasicEmail">
+                            <Form.Label>Email</Form.Label>
+                            <Form.Control type="Email" placeholder="Email" />
+                          </Form.Group>
+                          <div
+                            style={{
+                              display: "flex",
+                              justifyContent: "center",
+                              alignItems: "center"
+                            }}
+                          >
+                            <Button variant="dark" type="submit">
+                              Register
+                            </Button>
+                          </div>
+                        </Form>
+                      </Card.Body>
+                    </Card>
+                  </Tab.Pane>
+                </Tab.Content>
+              </Col>
+            </Row>
+          </Tab.Container>
         </div>
       </div>
     );
