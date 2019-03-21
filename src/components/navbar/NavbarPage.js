4dc6da7c9a5a19d95cb837b76b3dc9822a33a7a3
Kenneth Ngo <kenneth.ngo@no.experis.com>
2019-03-21 11:05:38 +0100

    Add Login Component

--- src/components/navbar/NavbarPage.js
+++ src/components/navbar/NavbarPage.js
@@ -1,27 +1,22 @@
 import React from "react";
 import { Navbar, Nav, NavDropdown } from "react-bootstrap";
-import { Route, NavLink, HashRouter, Router } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Login from "../login/login";
 
 class NavBarPage extends React.Component {
   render() {
     return (
-      <HashRouter>
-        <Navbar bg="primary" variant="dark" expand="lg">
-          <Navbar.Brand href="/">KPS</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ml-auto">
-              <Nav.Link href="">Search</Nav.Link>
-              <NavDropdown.Divider />
-              <Nav.Link href="/" to="/login">
-                Login
-              </Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
-        <Router />
-      </HashRouter>
+      <Navbar bg="primary" variant="dark" expand="lg">
+        <Navbar.Brand href="/">KPS</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto">
+            <Nav.Link href="">Search</Nav.Link>
+            <NavDropdown.Divider />
+            <Nav.Link href="/login">Login</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
     );
   }
 }
